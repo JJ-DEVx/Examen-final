69b43f70196da7ed838258b3fe96424b230aa764
JJ-DEVx <jhonvallarinoxx@gmail.com>
2026-04-19 13:25:54 -0500

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from database import db
+from flask_cors import CORS
+from routes import init_routes
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workshops.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+init_routes(app)
+
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
