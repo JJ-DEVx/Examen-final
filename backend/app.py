64084c0c93cae94ea6956ed5767196f9672f03d5
john <jhonvallarinoxx@gmail.com>
2026-04-19 19:15:54 -0500

    Fix SQLite Render

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ CORS(app)
 
 #  Ruta correcta para Render
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'workshops.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/workshops.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
